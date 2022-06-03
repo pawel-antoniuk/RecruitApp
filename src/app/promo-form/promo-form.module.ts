abbd2249bf8029efd1c6f782596dafee581d1489
pawel-antoniuk <pawelantoniuk@outlook.com>
2022-06-03 23:20:58 +0200

    Add DefinitionPage layout

--- src/app/promo-form/promo-form.module.ts
+++ src/app/promo-form/promo-form.module.ts
@@ -6,18 +6,29 @@ import {StepSummaryComponent} from './step-summary/step-summary.component';
 import {PromoFormComponent} from './promo-form/promo-form.component';
 import {RouterModule, Routes} from "@angular/router";
 import {PromoFormRoutingModule} from "./promo-form-routing.module";
-
+import { NavigationComponent } from './navigation/navigation.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatNativeDateModule} from "@angular/material/core";
+import {MatInputModule} from "@angular/material/input";
+import {MatRadioModule} from "@angular/material/radio";
+import {MatDatepickerModule} from "@angular/material/datepicker";
 
 @NgModule({
   declarations: [
     StepDefinitionComponent,
     StepPlaceholderComponent,
     StepSummaryComponent,
-    PromoFormComponent
+    PromoFormComponent,
+    NavigationComponent
   ],
   imports: [
     CommonModule,
-    PromoFormRoutingModule
+    PromoFormRoutingModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    MatRadioModule,
+    MatCheckboxModule,
   ],
   exports: [
     PromoFormComponent
